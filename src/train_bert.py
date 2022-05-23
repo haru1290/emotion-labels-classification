2bb64ec2208f5e85f72c0126d5de3435be9d4c70
S-Haruya <suzuki@ai.cs.ehime-u.ac.jp>
2022-05-23 16:30:47 +0900

    Update File

--- src/train_bert.py
+++ src/train_bert.py
@@ -38,12 +38,13 @@ class EarlyStopping:
         self.force_cancel = False
 
     def __call__(self, val_qwk, model):
-        score = -val_qwk
+        # score = -val_qwk
+        score = val_qwk
 
         if self.best_score is None:
             self.best_score = score
             self.save_checkpoint(val_qwk, model)
-        elif score > self.best_score:
+        elif score < self.best_score:
             self.counter += 1
             print(f"EarlyStopping counter: {self.counter} out of {self.patience}")
             if self.counter >= self.patience:
