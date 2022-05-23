4357f8497b1bcb5edca16867106f83964348eeef
S-Haruya <suzuki@ai.cs.ehime-u.ac.jp>
2022-05-23 17:11:24 +0900

    Update File

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -14,14 +14,19 @@ def main(args):
     valid.reset_index(drop=True, inplace=True)
     test.reset_index(drop=True, inplace=True)
 
-    train.to_csv('./data/train.tsv', index=False, sep='\t', encoding='utf_8_sig')
-    valid.to_csv('./data/valid.tsv', index=False, sep='\t', encoding='utf_8_sig')
-    test.to_csv('./data/test.tsv', index=False, sep='\t', encoding='utf_8_sig')
+    train.to_csv(args.train, index=False, sep='\t', encoding='utf_8_sig')
+    valid.to_csv(args.valid, index=False, sep='\t', encoding='utf_8_sig')
+    test.to_csv(args.test, index=False, sep='\t', encoding='utf_8_sig')
 
 
 if __name__ == '__main__':
     parser = ArgumentParser()
+
     parser.add_argument('--wrime', default='./data/wrime-ver2.tsv')
+    parser.add_argument('--train', default='./data/train.tsv')
+    parser.add_argument('--valid', default='./data/valid.tsv')
+    parser.add_argument('--test', default='./data/test.tsv')
+
     args = parser.parse_args()
 
     main(args)
