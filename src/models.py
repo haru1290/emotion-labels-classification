9cbf1e37b703a6be197e85430690464a8ce3737f
S-Haruya <suzuki@ai.cs.ehime-u.ac.jp>
2022-06-24 17:40:58 +0900

    Update File

--- src/models.py
+++ src/models.py
@@ -8,23 +8,20 @@ class BertWikiClassifier(nn.Module):
         super().__init__()
         self.bert = BertModel.from_pretrained(pretrained_model)
         self.drop = nn.Dropout(drop_rate)
-        self.classifier = nn.Linear(self.bert.config.hidden_size, n_classes)
+        # self.classifier = nn.Linear(self.bert.config.hidden_size, n_classes)
+        self.classifier = nn.Linear(1536, n_classes)
         self.mode = mode
 
     def forward(self, input_ids, attention_mask, user_features):
         _, pooler_output = self.bert(input_ids, attention_mask=attention_mask)
 
-        if self.mode == 'Sum':
-            output = pooler_output + user_features
-        elif self.mode == 'Sub':
-            output = pooler_output - user_features
-        elif self.mode == 'Product':
+        if self.mode == 'Product':
             output = pooler_output * user_features
         elif self.mode == 'Concat':
             output = torch.cat([pooler_output, user_features], dim=1)
         else:
             output = pooler_output
-
+            
         preds = self.classifier(self.drop(output))
 
         return preds
