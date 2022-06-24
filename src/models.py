8e10d275853cec8f2dc8b7d03effd81723ea40f4
S-Haruya <suzuki@ai.cs.ehime-u.ac.jp>
2022-06-24 14:36:30 +0900

    Update File

--- src/models.py
+++ src/models.py
@@ -16,4 +16,18 @@ class BertWikiClassifier(nn.Module):
         preds = self.classifier(self.drop(pooler_output))
 
         return preds
-        
+
+
+class BertSNSClassifier(nn.Module):
+    def __init__(self, n_classes: int, drop_rate, pretrained_model):
+        super().__init__()
+        self.bert = BertModel.from_pretrained(pretrained_model)
+        self.drop = nn.Dropout(drop_rate)
+        self.classifier = nn.Linear(self.bert.config.hidden_size, n_classes)
+
+    def forward(self, input_ids, attention_mask, user_features):
+        _, pooler_output = self.bert(input_ids, attention_mask=attention_mask)
+        # pooler_output = pooler_output * user_features
+        preds = self.classifier(self.drop(pooler_output))
+
+        return preds
