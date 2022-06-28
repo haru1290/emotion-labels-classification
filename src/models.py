fa06cc02fb90118dcdef6453674d82af04ae6c54
S-Haruya <suzuki@ai.cs.ehime-u.ac.jp>
2022-06-28 16:40:31 +0900

    Update File

--- src/models.py
+++ src/models.py
@@ -10,16 +10,23 @@ class BertWikiClassifier(nn.Module):
         self.drop = nn.Dropout(drop_rate)
         self.mode = mode
         if self.mode == 'Concat':
-            self.classifier = nn.Linear(1536, n_classes)
+            self.classifier = nn.Linear(self.bert.config.hidden_size * 4, n_classes)
         else:
             self.classifier = nn.Linear(self.bert.config.hidden_size, n_classes)
 
     def forward(self, input_ids, attention_mask, user_features):
         _, pooler_output = self.bert(input_ids, attention_mask=attention_mask)
-        if self.mode == 'Product':
+        if self.mode == 'Sum':
+            sequence_output = pooler_output + user_features
+        elif self.mode == 'Difference':
+            sequence_output = pooler_output - user_features
+        elif self.mode == 'Product':
             sequence_output = pooler_output * user_features
         elif self.mode == 'Concat':
-            sequence_output = torch.cat([pooler_output, user_features], dim=1)
+            sequence_output_sum = pooler_output + user_features
+            sequence_output_dif = pooler_output + user_features
+            sequence_output_pro = pooler_output * user_features
+            sequence_output = torch.cat([pooler_output, sequence_output_sum, sequence_output_dif, sequence_output_pro ], dim=1)
         else:
             sequence_output = pooler_output
         preds = self.classifier(self.drop(sequence_output))
